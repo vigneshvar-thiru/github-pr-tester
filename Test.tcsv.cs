9d8b646ae1bdf098a5a754b6e121e84512f2d70a
Vigneshvar Thiruvengadam <21968146@student.uwa.edu.au>
2026-01-28 16:49:49 +0800

    Changes

---
+++ Test.tcsv.cs
@@ -0,0 +1,13 @@
+!System.Customer
+CustomerId, Description, code
+1, Test1, T1
+2, Test2, T2
+3, Test3, T3
+4, Test4, T4
+
+
+! System.Product
+ProductId, ProductName, Price
+1001, ProductA,29.99
+1002, ProductB,49.99
+3, ProductC,19.99
